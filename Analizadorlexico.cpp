eea4bb0fac317cebd6fcf1c398dcebc15e730d0e
Rodrigo Levinski <r.levinski@live.com>
2015-03-16 21:26:40 -0300

    Adicionado leitura de arquivo

--- Analizadorlexico.cpp
+++ Analizadorlexico.cpp
@@ -2,10 +2,35 @@
 //
 
 #include "stdafx.h"
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <conio.h>
+#include <time.h>
+#include <algorithm>
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
+	ifstream infile("teste.txt");
+	string line;
+	//Leitura do arquivo
+	while (getline(infile, line)){
+		istringstream iss(line);
+		string n;
+		vector<string> v;
+
+		while (iss >> n){
+			v.push_back(n);
+		}
+
+	}
+	infile.close();
+	int para;
+	cin >> para;
 	return 0;
 }
 
